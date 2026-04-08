4b55e585c2fb4a2c325a965fe02c47e181fe4fad
zafarazlay <47980191+zafarazlay@users.noreply.github.com>
2026-04-08 03:26:28 -0800

    Add PKCE flow, rebuild and redeploy to docs/

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -13,5 +13,7 @@ export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABL
     storage: localStorage,
     persistSession: true,
     autoRefreshToken: true,
+    detectSessionInUrl: true,
+    flowType: 'pkce',
   }
 });
